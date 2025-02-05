1fa955afc7d409456ad83d060e1ed9de31f28930
parby8605 <lhj971011@gmail.com>
2025-02-05 17:55:04 +0900

    main 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  function convert(user) {
-    const converted_user = {}
-    converted_user.name = user.name
-    converted_user.age = user.age
-    if (user.address != null) {
-      converted_user.address = user.address
-    }
-
-    if (user.job != null) {
-      converted_user.job = user.job
-    }
-    return converted_user
-  }
-  const result = {
-    success: false,
-    body: [
-      {
-        name: 'Aaron',
-        age: 10,
-        address: null,
-      },
-      {
-        name: 'Baron',
-        age: 20,
-        address: {
-          location: 'Earth',
-        },
-      },
-      {
-        name: 'Caron',
-        age: 30,
-        address: null,
-        job: 'Developer',
-      },
-    ],
-  }
-  const converted_users = []
-  for (let i = 0; i < result.body.length; i++) {
-    const converted_user = convert(result.body[i])
-    converted_users.push(converted_user)
-  }
-  console.log(JSON.stringify(converted_users)) //문자로 변환 필요
-
-  return (
-    <>
-      <div>111111111</div>
-    </>
-  )
+  return <></>
 }
 
 export default App
